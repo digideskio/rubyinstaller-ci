5dbd8df9590412d31849c5f2deedca365bc4096e
Sam Kottler <shk@redhat.com>
2014-01-18 23:32:03 +0100

    Add the jenkins recipe to the site-local cookbook and bump the version (twice)

--- site-cookbooks/rubyinstaller/metadata.rb
+++ site-cookbooks/rubyinstaller/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "s@shk.io"
 license          "MIT"
 description      "rubyinstaller"
 long_description "Site-specific rubyinstaller stuff"
-version          "0.0.1"
+version          "0.0.3"
 
 recipe "users", "Adds the right users"
 
---
+++ site-cookbooks/rubyinstaller/recipes/jenkins.rb
@@ -0,0 +1,4 @@
+jenkins_plugin 'greenballs'
+jenkins_plugin 'github-oauth'
+jenkins_plugin 'git'
+jenkins_plugin 'ircbot'
